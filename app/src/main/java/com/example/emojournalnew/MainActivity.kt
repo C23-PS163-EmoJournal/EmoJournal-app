6ef5f08eae12331bf6e6a656a78b7af399fa3112
Dimas Wahyu Saputro <73456370+dimaswsss@users.noreply.github.com>
2023-06-16 13:36:59 +0700

    success add header and adivice

--- app/src/main/java/com/example/emojournalnew/MainActivity.kt
+++ app/src/main/java/com/example/emojournalnew/MainActivity.kt
@@ -9,18 +9,29 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.emojournal2.ApiConfig
 import com.example.emojournalnew.ui.theme.EmoJournalNewTheme
 import kotlinx.coroutines.Dispatchers
@@ -43,6 +54,29 @@ class MainActivity : ComponentActivity() {
     private val config = ApiConfig()
     private val emotionState = mutableStateOf("")
     private val confidenceState = mutableStateOf("")
+
+    val emotionEmojiMap = mapOf(
+        "angry" to R.drawable.angry,
+        "disgust" to R.drawable.disguised,
+        "sad" to R.drawable.sad,
+        "happy" to R.drawable.happy,
+        "neutral" to R.drawable.neutral,
+        "surprise" to R.drawable.surprise,
+        "fear" to R.drawable.fear,
+        // Add more emotions and their corresponding emoji images as needed
+    )
+
+    data class EmotionData(val emojiImageRes: Int, val adviceMessage: String)
+    val emotionDataMap = mapOf(
+        "angry" to EmotionData(R.drawable.angry, "Take a deep breath and count to ten before responding."),
+        "disgust" to EmotionData(R.drawable.disguised, "Find a positive distraction to shift your focus."),
+        "sad" to EmotionData(R.drawable.sad, "Reach out to someone you trust and share your feelings."),
+        "happy" to EmotionData(R.drawable.happy, "Celebrate your achievements and spread joy to others."),
+        "neutral" to EmotionData(R.drawable.neutral, "Take a moment for self-reflection and self-care."),
+        "surprise" to EmotionData(R.drawable.surprise, "Embrace the unexpected and stay open to new opportunities."),
+        "fear" to EmotionData(R.drawable.fear, "Challenge your fears and take small steps toward overcoming them.")
+        // Add more emotions, emoji images, and advice messages as needed
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -112,7 +146,6 @@ class MainActivity : ComponentActivity() {
                     response: retrofit2.Response<ResponseBody>
                 ) {
                     Log.d("succeed", response.raw().toString())
-                    hit_api()
                 }
 
                 override fun onFailure(call: retrofit2.Call<ResponseBody>, t: Throwable) {
@@ -162,14 +195,60 @@ class MainActivity : ComponentActivity() {
     }
     @Composable
     fun EmotionScreen(emotionState: String, confidenceState: String) {
+        val emotionData = emotionDataMap[emotionState]
+        val emojiImagePainter: Painter? = emotionData?.emojiImageRes?.let { painterResource(it) }
+
         Column(
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxSize()
                 .padding(horizontal = 32.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Emotion: $emotionState")
-            Text(text = "Confidence: $confidenceState")
+            // Application name at the top
+            Text(
+                text = "EmoJournal",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                ),
+                modifier = Modifier.padding(vertical = 16.dp)
+            )
+
+            Box(
+                modifier = Modifier.weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    emojiImagePainter?.let {
+                        Image(
+                            painter = it,
+                            contentDescription = "Emoji",
+                            modifier = Modifier.size(64.dp) // Adjust the size as needed
+                        )
+                    }
+                    Text(text = "Emotion: $emotionState")
+                    Text(text = "Confidence: $confidenceState")
+
+                    emotionData?.adviceMessage?.let {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                                .background(color = Color.LightGray, shape = RoundedCornerShape(8.dp))
+                                .padding(8.dp)
+                        ) {
+                            Text(
+                                text = "Advice: $it",
+                                modifier = Modifier.fillMaxWidth(),
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
